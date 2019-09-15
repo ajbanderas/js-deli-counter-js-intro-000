fed0df9b76917c45cfebda114b9901acf090229d
searching-form-5732 <kevin.downs04@gmail.com>
2019-09-15 21:23:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function takeANumber(place, name){
-  var welcome = `Welcome, ${name}. You are number ${place} in line.`;
-  return welcome;
+  var welcomeMessage = [];
+  for(var i = 0; i < place.length - 1; i++){
+    welcomeMessage.push(`Welcome, ${name[i]}. You are number ${place[i]} in line.`);
+    return welcomeMessage[i];
+  }
 }
