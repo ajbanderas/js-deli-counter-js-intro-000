8efc0b55e2957d5e1dbfcadba2b0bec236b7821b
searching-form-5732 <kevin.downs04@gmail.com>
2019-09-15 22:30:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 function takeANumber(current, next){
-  var counter = current.length;
-  for(var i = 0; i < next.length; i++){
-    counter++;
-    return `Welcome, ${next}. You are number ${counter} in line.`;
-  }
+  current.push(next);
+  var num = current.length;
+  return `Welcome, ${next}. You are number ${num} in line`;
 }
